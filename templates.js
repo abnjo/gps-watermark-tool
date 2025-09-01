7ac6b0d1572626e7f4916e27d5b7ae75e9de4e1d
abnjo <tzfeabnjo@gmail.com>
2025-09-01 15:53:51 +0530

    nuked everything and added js for json

---
+++ templates.js
@@ -0,0 +1,23 @@
+// This file contains the template data for the GPS Watermark Tool.
+// It defines a single global variable, 'templatesData', which is an array of objects.
+
+const templatesData = [
+  {
+    "name": "Main Construction Site",
+    "locationName": "123 Builder's Avenue",
+    "latitude": 40.7128,
+    "longitude": -74.0060
+  },
+  {
+    "name": "North Field Office",
+    "locationName": "North Field Trailer",
+    "latitude": 34.0522,
+    "longitude": -118.2437
+  },
+  {
+    "name": "Warehouse Depot",
+    "locationName": "Storage Unit #5",
+    "latitude": 41.8781,
+    "longitude": -87.6298
+  }
+];
